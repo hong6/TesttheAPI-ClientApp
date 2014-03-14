2ec579146b05d6440579d4079de8086e8283d9d1
hong6 <ccliu68@gmail.com>
2014-03-14 10:23:55 -0400

    Consume node API

--- js/models/model-rest-jsonp.js
+++ js/models/model-rest-jsonp.js
@@ -1,14 +1,3 @@
-/*
-    If you are using the sample RESTFul services I published on GitHub, use the following URLs...
-
-      - For the Node.js sample backend (available in https://github.com/ccoenraets/directory-rest-nodejs)
-        Use: http://localhost:3000/employees
-
-      - For the PHP sample backend (available in https://github.com/ccoenraets/directory-rest-php)
-        Use: /directory-rest-php/employees
-
- */
-
 directory.Employee = Backbone.Model.extend({
 
     urlRoot:"http://localhost:3000/employees",
